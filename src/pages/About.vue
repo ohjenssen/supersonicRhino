ea7d9591b0dc12b007f4008dda42adca6f91e2bd
Oskar Heming Jenssen <oss@watts.dk>
2025-09-16 17:53:15 +0200

    project setup

---
+++ src/pages/About.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>About</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Home</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+</template>
+
+<style scoped>
+    h1 {
+        background-color: red;
+    }
+</style>
