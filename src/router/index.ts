deb1717cb5c197aa91a77f37c3fa547aaac1dbc4
Oskar Heming Jenssen <oss@watts.dk>
2026-01-08 20:42:35 +0100

    removed redundant code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/pages/Home.vue';
 import About from '@/pages/About.vue';
+import Workout from '@/pages/Workout.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { 
-      path: '/', 
-      component: Home,
-      children: [
-
-      ]
-    },
+    { path: '/', component: Home },
+    { path: '/workout', component: Workout},
     { path: '/about', component: About },
   ],
 })
